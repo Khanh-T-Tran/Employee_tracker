9ffe5ed7e27cc58eba7b034686c292a51a3b5033
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-07-25 18:58:34 -0700

    keys

--- db/schema.sql
+++ db/schema.sql
@@ -4,15 +4,15 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL,
-    name VARCHAR(30) NOT NULL
-    PRIMARY KEY (id),
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DEC NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
@@ -22,7 +22,8 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    manager_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
