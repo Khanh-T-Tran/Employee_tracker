905c8809031afda473642b28baa002aa2517cede
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-07-28 18:28:30 -0700

    small error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
     if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
+    console.log('Connected to database successful!!');
     startApp();
 });
 
