ef16e4d4771843234474ad09889566b53a95c884
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-07-28 18:25:58 -0700

    function display app name and function prompt user 1st choice

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
+// install environmental variable env to prevent pushing sensitive info
+require('dotenv').config();
+
 // setting up connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'employee_db'
-  });
-
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'employee_db',
+});
 
 connection.connect(err => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
     startApp();
-  });
+});
 
 // Function prompt image showcase
 startApp = () => {
@@ -25,5 +27,38 @@ startApp = () => {
     console.log("*                                 *")
     console.log("***********************************")
     promptUser();
-  };
+};
 
+// prompt user for the first choice
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: ['View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+            ]
+        }
+    ])
+        .then((answers) => {
+            const { choices } = answers;
+
+            if (choices === "View all departments") {
+                showDepartments();
+            }
+
+            if (choices === "View all roles") {
+                showRoles();
+            }
+
+            if (choices === "No Action") {
+                connection.end()
+            };
+        });
+};
