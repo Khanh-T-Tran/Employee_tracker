ca1ccf8830553154885d73b122c40aef1e861c2c
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-08-01 19:56:22 -0700

    added function show department, show role, and show employee

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+// var db = require('mysql2-promise')();
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
@@ -6,28 +7,26 @@ const cTable = require('console.table');
 require('dotenv').config();
 
 // setting up connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'employee_db',
-});
-
-connection.connect(err => {
-    if (err) throw err;
-    console.log('Connected to database successful!!');
-    startApp();
-});
-
-// Function prompt image showcase
-startApp = () => {
-    console.log("***********************************")
-    console.log("*                                 *")
-    console.log("*        EMPLOYEE TRACKER         *")
-    console.log("*                                 *")
-    console.log("***********************************")
-    promptUser();
-};
+const query = sql => {
+    return new Promise((resolve, reject) => {
+        let connection = mysql.createConnection({
+            host: 'localhost',
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: 'employee_db',
+        });
+        connection.query(sql, (err, rows) => {
+            if (err) {
+                connection.end();
+                reject(err);
+            }
+            else {
+                connection.end();
+                resolve(rows);
+            }
+        });
+    });
+}
 
 // prompt user for the first choice
 const promptUser = () => {
@@ -50,12 +49,16 @@ const promptUser = () => {
             const { choices } = answers;
 
             if (choices === "View all departments") {
-                showDepartments();
+                showDepartments()
             }
 
             if (choices === "View all roles") {
                 showRoles();
             }
+
+            if (choices === "View all employees") {
+                showEmployees();
+              }
 
             if (choices === "No Action") {
                 connection.end()
@@ -63,14 +66,47 @@ const promptUser = () => {
         });
 };
 
+// function show all departments data
 function showDepartments() {
-    connection.query(`SELECT * FROM department`, function (err, results) {
-        if (err) {
-            console.log(err);
-        }
-        console.table(results);
-        startApp();
+    query(`SELECT * FROM department`).then(function (data) {
+       console.table(data);
+    }).then(function () {
+        promptUser();
     })
-
 }
 
+// function show all roles data
+function showRoles() {
+    query(`SELECT * FROM role`).then(function (data) {
+       console.table(data);
+    }).then(function () {
+        promptUser();
+    })
+}
+
+// function show all employees data
+function showEmployees() {
+    query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department,
+    role.salary,
+    CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+    FROM employee
+    LEFT JOIN role on employee.role_id = role.id
+    LEFT JOIN department on role.department_id = department.id
+    LEFT JOIN employee manager on manager.id = employee.manager_id`).then(function (data) {
+       console.table(data);
+    }).then(function () {
+        promptUser();
+    })
+}
+
+// Function prompt image showcase
+startApp = () => {
+    console.log("***********************************")
+    console.log("*                                 *")
+    console.log("*        EMPLOYEE TRACKER         *")
+    console.log("*                                 *")
+    console.log("***********************************")
+    promptUser();
+};
+
+startApp();
